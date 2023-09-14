202b9a80084daba26768e038d0d7f7fe56afc4e0
emthrology <emthrology@gmail.com>
2023-09-14 17:29:04 +0900

    using slots

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -3,12 +3,20 @@
   <div
     class="modal"
   >
-    <h1>This is a modal</h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae ipsa laboriosam vero natus ut rerum quaerat, saepe praesentium tempore et hic velit odio nemo minus labore quam ullam quod architecto?</p>
+    <h1><slot name='title' /></h1>
+    <slot />
     <button >Hide modal</button>
   </div>
 </teleport>
 </template>
+
+<script setup>
+  import {useSlots} from 'vue';
+
+  const slots = useSlots();
+
+  console.log(slots.title())
+</script>
 
 <style>
   .modal {
