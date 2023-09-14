d9d7a16ff1fb96c780daebf351eabb31a605a269
emthrology <emthrology@gmail.com>
2023-09-14 13:54:10 +0900

    using v-for

--- src/views/PostsView.vue
+++ src/views/PostsView.vue
@@ -1,21 +1,40 @@
-
+<script setup> 
+  /*
+    imports 
+  */
+  import {ref} from 'vue'
+  /*
+    posts
+  */
+  const posts = ref([
+    {
+      id: 'id1',
+      title: 'post 1',
+      content: 'post 1 content'
+    },
+    {
+      id: 'id2',
+      title: 'post 2',
+      content: 'post 2 content'
+    },
+    {
+      id: 'id3',
+      title: 'post 3',
+      content: 'post 3 content'
+    }
+  ])
+</script>
 <template>
   <div class="about">
    <h1>Posts</h1>
 
 
    <ul>
-    <li>
-      <RouterLink to="/postsDetail/id1">Post 1</RouterLink>
-    </li>
-    <li>
-      <RouterLink to="/postsDetail/id2">Post 2</RouterLink>
-    </li>
-    <li>
-      <RouterLink to="/postsDetail/id3">Post 3</RouterLink>
+    <li v-for="post in posts" :key="post.id">
+      <RouterLink :to="`/postsDetail/${post.id}`">{{ post.title }}</RouterLink>
     </li>
    </ul>
-   <textarea v-autofocus />
+   <textarea />
   </div>
  </template>
  
