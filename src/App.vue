b2d6351b540985366d0d52d0b15104ac07252ef2
emthrology <emthrology@gmail.com>
2023-09-14 10:52:58 +0900

    정리

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
 <template>
   <nav>
     <RouterLink to="/">Home</RouterLink>
-    <RouterLink to="/about">About</RouterLink>
+    <RouterLink to="/posts">Posts</RouterLink>
   </nav>
 
   <RouterView />
